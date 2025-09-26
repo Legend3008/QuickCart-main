3cee891149a925377705d7a9054f49964b49a9ab
Lalbabu Yadav <yesimyadav@gmail.com>
2025-09-27 01:50:13 +0530

    Fix typo pprocess → process

--- config/inngest.js
+++ config/inngest.js
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest";
 import connectDB from "./db";
+import User from "@/models/user";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickcart-next" });
@@ -7,16 +8,16 @@ export const inngest = new Inngest({ id: "quickcart-next" });
 // Inngest function to save user  data to database
 export const syncUserCreation = inngest.createFunction(
     {
-        id: 'sync-user-clerk'
+        id: 'sync-user-from-clerk'
     },
     { event: 'clerk/user.created' },
-    async ({ event }) => {
+    async ({event}) => {
         const { id, first_name, last_name, email_addresses, image_url } = event.data
         const userData = {
-            _id: id,
+            _id:id,
             email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
-            imageURL: image_url
+            imageUrl: image_url
         }
         await connectDB()
         await User.create(userData)
@@ -29,7 +30,7 @@ export const syncUserUpdation = inngest.createFunction(
         id: 'update-user-from-clerk'
     },
     { event: 'clerk/user.updated' },
-    async ({ event }) => {
+    async ({event}) => {
         const { id, first_name, last_name, email_addresses, image_url } = event.data
         const userData = {
             _id: id,
@@ -48,12 +49,13 @@ export const syncUserDeletion = inngest.createFunction(
         id: 'delete-user-with-clerk'
     },
     { event: 'clerk/user.deleted' },
-    async ({ event }) => {
+    async ({event}) => {
 
-        const { id } = event.data
+        const {id } = event.data
 
         await connectDB()
         await User.findByIdAndDelete(id)
     }
 
 )
+
